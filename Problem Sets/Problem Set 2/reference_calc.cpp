3d156f98157a89935539072014fb683f9c39b277
Bechir Zalila <bechir.zalila@gmail.com>
2021-01-20 16:20:56 +0100

    Homework lesson 02, continued...

--- Problem Sets/Problem Set 2/reference_calc.cpp
+++ Problem Sets/Problem Set 2/reference_calc.cpp
@@ -34,6 +34,11 @@ void channelConvolution(const unsigned char* const channel,
       channelBlurred[r * numCols + c] = result;
       if ((r * numCols + c) % 20000 == 0) {
 	printf ("RES2 %f. VAL %d\n", result, channel[r * numCols + c]);
+	for (int k = 0; k < filterWidth; k++) {
+	  printf ("%f ", filter[k]);
+	}
+	printf ("\n");
+	
       }
       /*if ((r * numCols + c) % 1000 == 0) {
 	printf ("RES2 %f\n", result);
