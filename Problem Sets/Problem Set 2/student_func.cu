c82224a2a17f5111092c6cd73141188b6dfd7196
Bechir Zalila <bechir.zalila@gmail.com>
2021-01-20 16:04:28 +0100

    Homework lesson 02, continued...

--- Problem Sets/Problem Set 2/student_func.cu
+++ Problem Sets/Problem Set 2/student_func.cu
@@ -156,11 +156,11 @@ void gaussian_blur(const unsigned char* const inputChannel,
     outputChannel[r * numCols + c] = result;
     if ((r * numCols + c) % 1000 == 0) {
       printf ("RES %f\n", result);
-      printf ("%d %d\n", inputChannel[r * numCols + c] ,outputChannel[r * numCols + c]);
-      for (int k = 0; k < filterWidth; k++) {
-	printf ("%5.2f ", filter[k]);
-      }
-      printf ("\n");
+      //printf ("%d %d\n", inputChannel[r * numCols + c] ,outputChannel[r * numCols + c]);
+      //for (int k = 0; k < filterWidth; k++) {
+      //	printf ("%5.2f ", filter[k]);
+      //}
+      //printf ("\n");
     }
   }
   
