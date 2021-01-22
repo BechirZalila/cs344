90e680a93b8e4012331b0fc7ff37a073c333fde6
Bechir Zalila <bechir.zalila@gmail.com>
2021-01-22 16:10:12 +0100

    Homework lesson 03, continued...

--- Problem Sets/Problem Set 3/student_func.cu
+++ Problem Sets/Problem Set 3/student_func.cu
@@ -225,14 +225,14 @@ __global__ void naive_scan(unsigned int *g_odata, unsigned int *g_idata, int n)
     temp[pout*n+thid] = temp[pin*n+thid];
   __syncthreads();
 
-  /*offset = 2;
+  offset = 2;
   pout = 1 - pout; // swap double buffer indices
   pin = 1 - pin;
   if (thid >= offset)
     temp[pout*n+thid] += temp[pin*n+thid - offset];
   else
     temp[pout*n+thid] = temp[pin*n+thid];
-    __syncthreads();*/
+    __syncthreads();
   
 
   /*for (int offset = 1; offset < n; offset *= 2) {
