0a448c5a00d6f381f0e1a5de63e6a3ca16b26913
Bechir Zalila <bechir.zalila@gmail.com>
2021-01-26 10:18:31 +0100

    Homework 04... continued

--- Problem Sets/Problem Set 4/student_func.cu
+++ Problem Sets/Problem Set 4/student_func.cu
@@ -42,6 +42,94 @@
 
  */
 
+__global__ void histo_kernel(unsigned int * d_out,
+			     unsigned int* const d_in,
+			     unsigned int shift,
+			     const unsigned int numElems)
+{
+  // Only 2 bins (one for zeroes and on for ones)
+  
+  unsigned int mask = 1 << shift;
+  
+  int myId = threadIdx.x + blockDim.x * blockIdx.x;
+
+  if (myId >= numElems)
+    return;
+
+  int bin = (d_in[myId] & mask) >> shift;
+  atomicAdd(&d_out[bin], 1);
+}
+
+// Blelloch Scan - described in lecture
+__global__ void sumscan_kernel(unsigned int * d_in,
+			       const size_t numBins,
+			       const unsigned int numElems)
+{
+  int myId = threadIdx.x;
+  
+  if (myId >= numElems)
+    return;
+
+  extern __shared__ float sdata[]; // Allocated on kernel call
+  
+  sdata[myId] = d_in[myId];
+  __syncthreads();            // make sure entire block is loaded!
+
+  for (int d = 1; d < numBins; d *= 2) {
+    if (myId >= d) {
+      sdata[myId] += sdata[myId - d];
+    }
+    __syncthreads();
+  }
+  
+  if (myId == 0)
+    d_in[0] = 0;
+  else
+    d_in[myId] = sdata[myId - 1]; //inclusive->exclusive
+}
+
+__global__ void makescan_kernel(unsigned int * d_in,
+				unsigned int *d_scan,
+				unsigned int shift,
+				const unsigned int numElems)
+{
+  unsigned int mask = 1 << shift;
+  int myId = threadIdx.x + blockDim.x * blockIdx.x;
+  
+  if (myId >= numElems)
+    return;
+
+  d_scan[myId] = ((d_in[myId] & mask) >> shift) ? 0 : 1;
+}
+
+__global__ void move_kernel(unsigned int* const d_inputVals,
+			    unsigned int* const d_inputPos,
+			    unsigned int* const d_outputVals,
+			    unsigned int* const d_outputPos,
+			    const unsigned int numElems,
+			    unsigned int* const d_histogram,
+			    unsigned int* const d_scaned,
+			    unsigned int shift)
+{
+  unsigned int mask = 1 << shift;
+  int myId = threadIdx.x + blockDim.x * blockIdx.x;
+  
+  if (myId >= numElems)
+    return;
+
+  // Algorithm described in 7.4 of http://wykvictor.github.io/2016/04/03/Cuda-2.html 
+
+  int des_id = 0;
+
+  if ((d_inputVals[myId] & mask) >> shift) {
+    des_id = myId + d_histogram[1] - d_scaned[myId];
+  } else {
+    des_id = d_scaned[myId];
+  }
+  
+  d_outputVals[des_id] = d_inputVals[myId];
+  d_outputPos[des_id] = d_inputPos[myId];
+}
 
 void your_sort(unsigned int* const d_inputVals,
                unsigned int* const d_inputPos,
@@ -49,6 +137,5 @@ void your_sort(unsigned int* const d_inputVals,
                unsigned int* const d_outputPos,
                const size_t numElems)
 { 
-  //TODO
-  //PUT YOUR SORT HERE
+  
 }
