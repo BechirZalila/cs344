36d9968032a31a2ca786aa5c6c14fa10882ea7b6
Bechir Zalila <bechir.zalila@gmail.com>
2021-02-02 14:13:48 +0100

    Homework 6... cont

--- Problem Sets/Problem Set 6/reference_calc.cpp
+++ Problem Sets/Problem Set 6/reference_calc.cpp
@@ -199,6 +199,7 @@ void reference_calc(const uchar4* const h_sourceImg,
   
   // debug
 
+  printf ("Ref. : ");
   for (int j = 0, k = 0; (j < numRowsSource * numColsSource) && (k < 100) ; j++) {
     if (strictInteriorPixels [j]) {
       printf ("%2.2f ", blendedValsRed_1[j]);
