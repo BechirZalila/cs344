db580a18662d833fdcc79f8300e14df3a009a9c2
Bechir Zalila <bechir.zalila@gmail.com>
2021-02-05 15:01:43 +0100

    Lesson 7 snippet... cont

--- Lesson Code Snippets/Lesson 7 Code Snippets/tiling/tiling.cu
+++ Lesson Code Snippets/Lesson 7 Code Snippets/tiling/tiling.cu
@@ -128,16 +128,17 @@ int main(int argc, char **argv)
   foo<<<N/BLOCKSIZE, BLOCKSIZE>>>
     (d_fooOut, d_fooA, d_fooB, d_fooC, d_fooD, d_fooE);
   fooTimer.Stop();
+
+  cudaMemcpy(fooOut, d_fooOut, numBytes, cudaMemcpyDeviceToHost);
+  printf("foo<<<>>>(): %g ms elapsed. Verifying solution...",
+	 fooTimer.Elapsed());
+  compareArrays(ref_fooOut, fooOut, N);
   
   barTimer.Start();
   bar<<<N/BLOCKSIZE, BLOCKSIZE>>>(d_barOut, d_barIn);
   barTimer.Stop();
   
-  cudaMemcpy(fooOut, d_fooOut, numBytes, cudaMemcpyDeviceToHost);
   cudaMemcpy(barOut, d_barOut, numBytes, cudaMemcpyDeviceToHost);
-  printf("foo<<<>>>(): %g ms elapsed. Verifying solution...",
-	 fooTimer.Elapsed());
-  compareArrays(ref_fooOut, fooOut, N);
   printf("bar<<<>>>(): %g ms elapsed. Verifying solution...",
 	 barTimer.Elapsed());
   compareArrays(ref_barOut, barOut, N);
